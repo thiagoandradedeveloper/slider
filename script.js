0619216034a06ffe0795fb248a842c1814bd5815
thiagoandradedeveloper <thiagoha3@gmail.com>
2023-06-05 23:42:29 -0300

    finalizado 100%

--- script.js
+++ script.js
@@ -1,191 +1,147 @@
 window.onload = function(){
 
     let posicao = 0;
+    let proxima;
+    let containerAll = document.querySelectorAll(".containerImg").item(0);
+    containerAll.innerHTML += `
+        <div id="containerMiniatura"></div>
+        <button class="btnsControler voltar">&#9668;</button>
+        <button class="btnsControler avancar">&#9658;</button>
+    `;
     let imagens = document.querySelectorAll(".containerImg img");
-    let mudar = true;
+    let btns = document.querySelectorAll(".containerImg button");
+    let mudar = mudar2 = true;
     let time  = 2000;
-    let altomatico = setInterval(voltar,time++);
-    let atual = 0;
 
-    document.querySelectorAll(".containerImg").item(0).addEventListener("mousemove",()=>{
-        clearInterval(altomatico)
-    })
-    document.querySelectorAll(".containerImg").item(0).addEventListener("mouseover",()=>{
-        clearInterval(altomatico)
-    })
-    document.querySelectorAll(".containerImg").item(0).addEventListener("mouseout",()=>{
-        altomatico = setInterval(voltar,time++)
-    })
+    for(img of imagens){ img.style.left = "-100%" }
+    imagens[posicao].style.transition = '0s';
+    imagens[posicao].style.left = '0';
+
+    function ajusteValor(valor){
+        if(valor < 0){ valor = imagens.length - 1 }
+        if(valor > imagens.length -1){ valor = 0 }
+        return valor
+    }
+
+    btns[0].onclick = () => voltar()
+    btns[1].onclick = () => avancar()
     
-    let containerMiniatura = document.getElementById("containerMiniatura")
-    conteudo = ""
-    contador = 0
-    for(image of imagens){
-        if(image != imagens.item(posicao)){ image.style.left = "-600px"; }
-        else {
-            imagens.item(0).style.left = 0;
+    function avancar(next){
+
+        let contador1 = 0;
+        for(img of imagens){
+            if(img.style.left == "0px") {
+                posicao = contador1
+                img.style.zIndex = '2'
+            } else {
+                img.style.zIndex = '1'
+            }
+
+            contador1++;
         }
-        conteudo += `
-            <div class='divMini' id='dm${contador}'>
-                <div class='imgMini' style="background:url('${image.src}');background-position: 0 0;background-repeat: no-repeat;background-size: 100% 100%;"></div>
-                ${contador+1}
-            </div>`
-        contador++
-    }
-    containerMiniatura.innerHTML += conteudo
-    document.getElementById('dm0').style.border = "thin solid red"
+        //alert(" atual " + atual + " posicao " + posicao + " proxima " + proxima + " next " + next )
+        if(next == posicao) mudar2 = false
+        else mudar2 = true
+        
+        if(mudar && mudar2){
 
-    let divsMini = document.querySelectorAll('.divMini')
-    for(div of divsMini){
-        div.addEventListener('click',(e) => {
-            mover(e.srcElement.id.replace("dm",""));
-        })
-    }
+            mudar = false
 
-    let btnCotrolers = document.querySelectorAll(".containerImg button")
-    btnCotrolers[0].onclick = () => {
-        clearInterval(altomatico)
-        avancar()        
-    }
-    btnCotrolers[1].onclick = () => {
-        clearInterval(altomatico)
-        voltar()
-    }
-    function mover(imgId){
-
-        if(mudar){
-
-            clearInterval(altomatico)
-
-            mudar = false;
-
-            imagens.item(atual).style.transition = time + 'ms';
-            imagens.item(atual).style.left = '-600px';
-
-            imagens.item(imgId).style.transition = '0ms';
-            imagens.item(imgId).style.left = '600px';
-
-            setTimeout(function(){ 
-                imagens.item(imgId).style.transition = time + 'ms';
-                imagens.item(imgId).style.left = '0px';
-                    
-                atual = imgId
-                imgId++
-                
-                if(imgId > imagens.length -1) imgId = 0
-                console.log(imgId)
-                
-                posicao = imgId
-                mudar = true
-
-            },0)
-
-
-
-            /*for(image of imagens){
-
-                if(image != imagens.item(atual)){
-                    image.style.transition = '0ms';
-                    image.style.left = '600px';
-                }
+            if(next == undefined){
+                proxima = ajusteValor(posicao + 1)
+            } else {
+                proxima = next.replace('dm','')
             }
 
-            setTimeout(function(){
+            imagens[posicao].style.transition = '0s';
+            imagens[posicao].style.left = '0';
+            imagens[proxima].style.transition = '0s';
+            imagens[proxima].style.left = '100%';
 
-                imagens.item(atual).style.transition = time + 'ms';
-                imagens.item(atual).style.left = '-600px';
+            let botoesGuia = document.querySelectorAll('.divMini')
+            for(botao of botoesGuia){ botao.style.border = "thin solid blue" }
+            botoesGuia[proxima].style.border = "thin solid red"
 
-                imagens.item(imgId).style.transition = time + 'ms';
-                imagens.item(imgId).style.left = '0px';
-
+            setTimeout(()=>{
+                imagens[posicao].style.transition = '1s';
+                imagens[posicao].style.left = '-100%';
+                imagens[proxima].style.transition = '1s';
+                imagens[proxima].style.left = '0';
+                posicao = ajusteValor(posicao + 1);
             },1)
-
-            setTimeout(function(){ 
-                imagens.item(posicao).style.transition = '0ms';
-                mudar = true; 
-            },time++)
-
-            console.log("Atual: " + atual + "Index: " + imgId)
-
-            atual = imgId*/
-        }
-    }
-    function avancar(){
-
-        if(mudar){
-
-            mudar = false;
-            
-            for(image of imagens){
-                image.style.transition = '0s';
-            }
-            for(image of imagens){
-                if(image != imagens.item(posicao)){ 
-                    image.style.left = "-600px";
-                }
-                document.getElementById('dm'+(posicao)).style.border = "thin solid blue"
-            }
-            setTimeout(function(){
-
-                imagens.item(posicao).style.transition = time + 'ms';
-                imagens.item(posicao).style.left = '600px';
-
-                posicao--            
-                if(posicao < 0) posicao = imagens.length - 1
-
-                document.getElementById('dm'+(posicao)).style.border = "thin solid red"
-                
-                imagens.item(posicao).style.transition = time + 'ms';
-                imagens.item(posicao).style.left = '0px';
-
-                atual = posicao
-
-            },1)
-            setTimeout(function(){ 
-                imagens.item(posicao).style.transition = '0ms';
-                mudar = true; 
-            },time++)
+            setTimeout(()=>mudar = true,time+1)           
         }
     }
     function voltar(){
 
-        if(mudar){            
+        let contador1 = 0;
+        for(img of imagens){
+            if(img.style.left == "0px") posicao = contador1
+            contador1++;
+        }
 
-            mudar = false;
-            
-            for(image of imagens){
-                image.style.transition = '0s';
-            }
+        if(mudar){
 
-            for(image of imagens){
-                if(image != imagens.item(posicao)) image.style.left = "600px";
-                document.getElementById('dm'+(posicao)).style.border = "thin solid blue"
-            }
-            setTimeout(function(){
+            mudar = false
 
-                imagens.item(posicao).style.transition = time + 'ms';
-                imagens.item(posicao).style.left = '-600px';
-                
-                posicao++            
-                if(posicao > imagens.length - 1) posicao = 0                
-                
-                document.getElementById('dm'+(posicao)).style.border = "thin solid red"
+            proxima = ajusteValor(posicao - 1)
 
-                imagens.item(posicao).style.transition = time + 'ms';
-                imagens.item(posicao).style.left = '0px';
+            imagens[posicao].style.transition = '0s';
+            imagens[posicao].style.left = '0';
+            imagens[proxima].style.transition = '0s';
+            imagens[proxima].style.left = '-100%';
 
-                atual = posicao
+            let botoesGuia = document.querySelectorAll('.divMini')
+            for(botao of botoesGuia){ botao.style.border = "thin solid blue" }
+            botoesGuia[proxima].style.border = "thin solid red"
 
+            setTimeout(()=>{
+                imagens[posicao].style.transition = '1s';
+                imagens[posicao].style.left = '100%';
+                imagens[proxima].style.transition = '1s';
+                imagens[proxima].style.left = '0';
+                posicao = ajusteValor(posicao - 1);
             },1)
-            setTimeout(function(){
-                imagens.item(posicao).style.transition = '0ms';
-                mudar = true; 
-            },time++)
+            setTimeout(()=>mudar = true,time+1)            
+        }
+    }
+    
+    let containerMiniatura = document.getElementById("containerMiniatura")
+    conteudo = ""
+    contador = 0
+    
+    for(image of imagens){
+        
+        conteudo += `
+            <div class='divMini' id='dm${contador}'>
+                <div class='imgMini' style="
+                    background:url('${image.src}');
+                    background-position: 0 0;
+                    background-repeat: no-repeat;
+                    background-size: 100% 100%;
+                "></div>
+                ${contador+1}
+            </div>
+        `;
+
+        contador++
+    }
+    
+    containerMiniatura.innerHTML = conteudo
+    document.getElementById('dm0').style.border = "thin solid red"
+
+    let botoesGuia = document.querySelectorAll('.divMini')
+    for(botao of botoesGuia){
+        
+        botao.onclick = function(e){
+            for(botao of botoesGuia){ botao.style.border = "thin solid blue" }
+            avancar(e.srcElement.id.replace('dm',''))
+            this.style.border = "thin solid red"
         }
     }
 
     document.onkeyup = (e) =>{ 
-        if(e.key == "ArrowRight"){ voltar() }
-        if(e.key == "ArrowLeft"){ avancar() }    
+        if(e.key == "ArrowRight"){ avancar() }
+        if(e.key == "ArrowLeft") { voltar()  }   
     }    
 }
